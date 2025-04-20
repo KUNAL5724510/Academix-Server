78c03c9ca3e035f0fbb3e9321d54ef5f730d292b
Ashu SHARMA <itcodetikki@gmail.com>
2025-04-20 14:04:58 +0530

    Login and Register done

--- src/main/java/academix/com/academix/Controller/CPrincipal.java
+++ src/main/java/academix/com/academix/Controller/CPrincipal.java
@@ -1,13 +1,18 @@
 package academix.com.academix.Controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import academix.com.academix.Repository.UserRepository;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import academix.com.academix.Entity.Principal;
 
 
@@ -23,5 +28,31 @@ public class CPrincipal {
          cuser.save(pdata);
          return "Principal Registered Successfully";
      }
+
+     @RequestMapping("/PLogin")
+        public String PrincipalLogin(@RequestBody Principal pdata) {
+            String email = pdata.getEmail();
+            String password = pdata.getPassword();
+
+            Optional<Principal> usermail = cuser.findByEmail(email);
+
+            if (usermail.isPresent()) {
+                Principal user = usermail.get();
+
+                if("PRINCIPAL".equalsIgnoreCase(user.getRole()))
+                 {
+                    if (user.getPassword().equals(password)) {
+                        return "Login Successful";
+                    } else {
+                        return "Invalid Password";
+                    }
+                } else {
+                    return "Invalid Role";
+                }
+                
+            } else {
+                return "Email not found";
+            }
+        }
      
 }
--- src/main/java/academix/com/academix/Repository/UserRepository.java
+++ src/main/java/academix/com/academix/Repository/UserRepository.java
@@ -3,10 +3,13 @@ package academix.com.academix.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import academix.com.academix.Entity.Principal;
 
 @Repository
 public interface UserRepository extends JpaRepository<Principal, Integer> {
+
+    Optional<Principal> findByEmail(String email);
   
 
 }
